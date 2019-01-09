c8fc14c68a8670f031b10d6e409a068ddfe7b068
Deliman206 <smiller206@me.com>
2019-01-09 13:44:24 -0800

    added try catch to methods

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -14,7 +14,8 @@ namespace ATM
 
         static int Menu(int accountBalance)
         {
-            Console.WriteLine("What kind of transaction would you like to make?/n");
+            
+            Console.WriteLine("What kind of transaction would you like to make?\n");
             Console.WriteLine("1. View Balance\n2. Withdraw\n3. Deposit\n4. Exit");
             string transactionType = Console.ReadLine();
 
@@ -47,24 +48,42 @@ namespace ATM
 
         public static int _ViewBalance(int balance)
         {
-            Console.WriteLine($"Your balance is {balance}");
-            return balance;
+            try
+            {
+                Console.WriteLine($"Your balance is {balance}");
+                return balance;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
         }
         static int ViewBalance(int accountBalance)
         {
+            Console.Clear();
             Menu(_ViewBalance(accountBalance));
             return accountBalance;
         }
 
         public static int _Withdraw(int accountBalance, int withdrawAmount)
         {
-            if (accountBalance >= withdrawAmount)
+            try
             {
-                accountBalance -= withdrawAmount;
+                if (accountBalance >= withdrawAmount)
+                {
+                    accountBalance -= withdrawAmount;
+                    return accountBalance;
+                }
+                Console.WriteLine("insufficient funds");
                 return accountBalance;
             }
-            Console.WriteLine("insufficient funds");
-            return accountBalance;
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         static int Withdraw(int accountBalance)
@@ -72,13 +91,23 @@ namespace ATM
             Console.WriteLine("How much would you like to withdraw?");
             string amount = Console.ReadLine();
             Int32.TryParse(amount, out int withdrawAmount);
+            //Console.Clear();
+            //seperate the line below so i can print the remaining balance after the withdraw
             return Menu(_Withdraw(accountBalance, withdrawAmount));
         }
 
         public static int _Deposit(int accountBalance, int depositAmount)
         {
-            accountBalance += depositAmount;
-            return accountBalance;
+            try
+            {
+                accountBalance += depositAmount;
+                return accountBalance;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         static int Deposit(int accountBalance)
@@ -86,6 +115,8 @@ namespace ATM
             Console.WriteLine("How much would you like to deposit?");
             string amount = Console.ReadLine();
             Int32.TryParse(amount, out int depositAmount);
+            //Console.Clear();
+            //seperate the line below so i can print the remaining balance after the Deposit
             return Menu(_Deposit(accountBalance, depositAmount));
         }
 
