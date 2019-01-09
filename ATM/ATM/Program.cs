40abc48bb0f71bb61410cabe97324cfe68c8ec07
Deliman206 <smiller206@me.com>
2019-01-09 13:29:20 -0800

    added 2 tests per method

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ATM
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -14,6 +11,7 @@ namespace ATM
             Console.WriteLine("Welcome to ATM");
             Menu(accountBalance);
         }
+
         static int Menu(int accountBalance)
         {
             Console.WriteLine("What kind of transaction would you like to make?/n");
@@ -46,33 +44,51 @@ namespace ATM
                     return Menu(accountBalance);
             }
         }
+
+        public static int _ViewBalance(int balance)
+        {
+            Console.WriteLine($"Your balance is {balance}");
+            return balance;
+        }
         static int ViewBalance(int accountBalance)
         {
-            Console.WriteLine($"Your balance is {accountBalance}");
-            Menu(accountBalance);
+            Menu(_ViewBalance(accountBalance));
             return accountBalance;
         }
+
+        public static int _Withdraw(int accountBalance, int withdrawAmount)
+        {
+            if (accountBalance >= withdrawAmount)
+            {
+                accountBalance -= withdrawAmount;
+                return accountBalance;
+            }
+            Console.WriteLine("insufficient funds");
+            return accountBalance;
+        }
+
         static int Withdraw(int accountBalance)
         {
             Console.WriteLine("How much would you like to withdraw?");
             string amount = Console.ReadLine();
             Int32.TryParse(amount, out int withdrawAmount);
-            if (accountBalance >= withdrawAmount)
-            {
-                accountBalance -= withdrawAmount;
-                return Menu(accountBalance);
-            }
-            Console.WriteLine("insufficient funds");
-            return Menu(accountBalance);
+            return Menu(_Withdraw(accountBalance, withdrawAmount));
         }
+
+        public static int _Deposit(int accountBalance, int depositAmount)
+        {
+            accountBalance += depositAmount;
+            return accountBalance;
+        }
+
         static int Deposit(int accountBalance)
         {
             Console.WriteLine("How much would you like to deposit?");
             string amount = Console.ReadLine();
             Int32.TryParse(amount, out int depositAmount);
-            accountBalance += depositAmount;
-            return Menu(accountBalance);
+            return Menu(_Deposit(accountBalance, depositAmount));
         }
+
         static int Exit(int accountBalance)
         {
             Environment.Exit(1);
---
+++ ATM/ATM_tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Xunit;
+using ATM;
+
+namespace ATM_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestViewBalanceMethod()
+        {
+            Assert.Equal(0, Program._ViewBalance(0));
+            Assert.Equal(10, Program._ViewBalance(10));
+        }
+        [Fact]
+        public void TestWithdrawMethod()
+        {
+            Assert.Equal(100, Program._Withdraw(100, 200));
+            Assert.Equal(100, Program._Withdraw(200, 100));
+        }
+        [Fact]
+        public void TestDepositMethod()
+        {
+            Assert.Equal(200, Program._Deposit(100, 100));
+            Assert.NotEqual(200, Program._Deposit(100,0));
+        }
+    }
+}
